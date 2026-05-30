dc05ae764c8926870142d2d732b762a0c1360391
Elkeid-me <elkeid.me@gmail.com>
2026-05-30 23:31:09 +0800

    LeNet5 C++ 未优化版

--- LeNet5/lenet5.cxx
+++ LeNet5/lenet5.cxx
@@ -1,7 +1,11 @@
 #include "lenet5_weights.hxx"
+#include <algorithm>
+#include <bit>
+#include <fstream>
 #include <iostream>
 #include <mdspan>
-#include <ranges>
+#include <memory>
+#include <string>
 #include <vector>
 
 template <std::size_t... Extents>
@@ -22,10 +26,9 @@ conv(const_float_mdspan<input_channels, input_height, input_width> input,
      const_float_mdspan<output_channels> bias)
 {
     static_assert(kernel_size % 2 == 1, "Only odd kernel sizes are supported.");
-    std::vector<float> output;
     constexpr std::size_t output_height{input_height - kernel_size + 1};
     constexpr std::size_t output_width{input_width - kernel_size + 1};
-    output.reserve(input_channels * output_height * output_width);
+    std::vector<float> output(output_channels * output_height * output_width);
     float_mdspan<output_channels, output_height, output_width> output_mdspan{
         output.data()};
     for (std::size_t oc{0}; oc < output_channels; oc++)
@@ -68,8 +71,7 @@ max_pooling(const_float_mdspan<input_channels, input_height, input_width> input)
 {
     constexpr std::size_t output_height{input_height / 2};
     constexpr std::size_t output_width{input_width / 2};
-    std::vector<float> output;
-    output.reserve(input_channels * output_height * output_width);
+    std::vector<float> output(input_channels * output_height * output_width);
     float_mdspan<input_channels, output_height, output_width> output_mdspan{
         output.data()};
     for (std::size_t c{0}; c < input_channels; c++)
@@ -86,9 +88,23 @@ max_pooling(const_float_mdspan<input_channels, input_height, input_width> input)
     return output;
 }
 
+template <std::size_t input_size, std::size_t output_size>
+std::vector<float> linear(std::vector<float> input,
+                          const_float_mdspan<output_size, input_size> weights,
+                          const_float_mdspan<output_size> bias)
+{
+    std::vector<float> output(output_size);
+    for (std::size_t o{0}; o < output_size; o++)
+    {
+        float sum{bias[o]};
+        for (std::size_t i{0}; i < input_size; i++)
+            sum += input[i] * weights[o, i];
+        output[o] = sum;
+    }
+    return output;
+}
 
-
-int lennet(const_float_mdspan<1, 28, 28> input)
+int lenet(const_float_mdspan<1, 32, 32> input)
 {
     // 0
     const_float_mdspan<6, 1, 5, 5> conv0_weights{features_0_weight};
@@ -97,7 +113,6 @@ int lennet(const_float_mdspan<1, 28, 28> input)
     std::vector<float> relu0_output{relu(std::move(conv0_output))};
     std::vector<float> pool0_output{
         max_pooling(const_float_mdspan<6, 28, 28>(relu0_output.data()))};
-
     // 1
     const_float_mdspan<16, 6, 5, 5> conv1_weights{features_3_weight};
     const_float_mdspan<16> conv1_bias{features_3_bias};
@@ -106,23 +121,50 @@ int lennet(const_float_mdspan<1, 28, 28> input)
              conv1_bias)};
     std::vector<float> relu1_output{relu(std::move(conv1_output))};
     std::vector<float> pool1_output{
-        max_pooling(const_float_mdspan<16, 5, 5>(relu1_output.data()))};
-
-    // // 2
-    // const_float_mdspan<120, 16, 5, 5> conv2_weights{features_6_weight};
-    // const_float_mdspan<120> conv2_bias{features_6_bias};
-    // std::vector<float> conv2_output{
-    //     conv(const_float_mdspan<16, 2, 2>(pool1_output.data()), conv2_weights,
-    //          conv2_bias)};
-
-    const_float_mdspan<84, 120> fc0_weights{classifier_1_weight};
-    const_float_mdspan<84> fc0_bias{classifier_1_bias};
-    const_float_mdspan<10, 84> fc1_weights{classifier_3_weight};
-    const_float_mdspan<10> fc1_bias{classifier_3_bias};
+        max_pooling(const_float_mdspan<16, 10, 10>(relu1_output.data()))};
+    // --- Flatten ---
+    // 0
+    const_float_mdspan<120, 400> fc0_weights{classifier_1_weight};
+    const_float_mdspan<120> fc0_bias{classifier_1_bias};
+    std::vector<float> fc0_output{
+        relu(linear(std::move(pool1_output), fc0_weights, fc0_bias))};
+    // 1
+    const_float_mdspan<84, 120> fc1_weights{classifier_3_weight};
+    const_float_mdspan<84> fc1_bias{classifier_3_bias};
+    std::vector<float> fc1_output{
+        relu(linear(std::move(fc0_output), fc1_weights, fc1_bias))};
+    // 2
+    const_float_mdspan<10, 84> fc2_weights{classifier_5_weight};
+    const_float_mdspan<10> fc2_bias{classifier_5_bias};
+    std::vector<float> fc2_output{
+        linear(std::move(fc1_output), fc2_weights, fc2_bias)};
+    return std::max_element(fc2_output.begin(), fc2_output.end()) -
+           fc2_output.begin();
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    static_assert(sizeof(float) == 4, "This code assumes float is 4 bytes.");
+    static_assert(std::endian::native == std::endian::little,
+                  "This code assumes little-endian architecture.");
+    if (argc != 2 && argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <input_file> <img_index>"
+                  << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::ifstream input_file(argv[1],
+                             std::ios_base::in | std::ios_base::binary);
+    if (!input_file)
+    {
+        std::cerr << "Error: Could not open input file." << std::endl;
+        return EXIT_FAILURE;
+    }
+    auto input{std::make_unique<float[]>(32 * 32)};
+    input_file.seekg(argc == 3 ? std::stoul(argv[2]) * 32 * 32 * sizeof(float)
+                               : 0,
+                     std::ios_base::beg);
+    input_file.read(reinterpret_cast<char *>(input.get()),
+                    32 * 32 * sizeof(float));
+    std::cout << lenet(const_float_mdspan<1, 32, 32>(input.get())) << std::endl;
 }
